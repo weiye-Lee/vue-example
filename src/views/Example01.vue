0bee468c7e3da14825ba2fab5e6966fffc7ce63f
doyen <weiye_lee@163.com>
2021-03-14 10:32:56 +0800

    新增example01 组件

---
+++ src/views/Example01.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="app">
+    <!-- 点击文本，文字发生变化，数据绑定 -->
+    <h1 @click="change">{{ message }}</h1>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  data: () => {
+    return {
+      message: "hello"
+    };
+  },
+  methods: {
+    change(): string {
+      this.message = this.message.replace("l", "$");
+      return this.message;
+    }
+  }
+});
+</script>
